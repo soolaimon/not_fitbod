c745d91935a1f53c04a2c10ca8d250a4a1557446
David Nelson <davidicus21@gmail.com>
2021-03-15 21:59:19 -0600

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv = CSV.open("./db/workout_data.csv")
+
+csv.each do |row|
+  user = User.where(email_address: row[0]).first_or_create
+  user.workouts.where(date: DateTime.parse(row[1]), duration: row[2].to_i).first_or_create
+end
